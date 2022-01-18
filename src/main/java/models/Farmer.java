3237de6789c3ff13da3ab76f668f8f4e1ec5449b
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-18 09:57:17 +0300

    Created basic POJO of the Farmer class

--- src/main/java/models/Farmer.java
+++ src/main/java/models/Farmer.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Farmer {
+    private String name;
+    private String location;
+    private String number;
+    private String produce;
+    private String email;
+    private int priceOfProduce;
+
+    public Farmer(String name, String location, String number, String produce, int priceOfProduce) {
+        this.name = name;
+        this.location = location;
+        this.number = number;
+        this.produce = produce;
+        this.email = "No email available";
+        this.priceOfProduce = priceOfProduce;
+    }
+
+    public Farmer(String name, String location, String number, String produce, String email, int priceOfProduce) {
+        this.name = name;
+        this.location = location;
+        this.number = number;
+        this.produce = produce;
+        this.email = email;
+        this.priceOfProduce = priceOfProduce;
+    }
+
+    pub
 }

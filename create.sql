5d15b6343a40a19e23e45da2d94b1e91df4b4de7
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-01-19 09:57:03 +0300

    Created reviews database table and homepage for the website

--- create.sql
+++ create.sql
@@ -5,5 +5,9 @@ CREATE DATABASE farmer_global;
 CREATE TABLE farmers (
 id SERIAL PRIMARY KEY, name varchar, location varchar, number varchar, produce varchar, amountOfProduceInKg int, email varchar, priceOf1kgOfProduce int, wallet int);
 
-CREATE DATABASE farmer_global_test WITH TEMPLATE farmer_global;
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY, farmername varchar, writtenby varchar, content varchar
+);
+
+-- CREATE DATABASE farmer_global_test WITH TEMPLATE farmer_global;
 
